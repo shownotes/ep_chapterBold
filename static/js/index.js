3515e678a5424f127d869c8cccd11e1ca36fee6a
Dr4k3 <dr4k3@gmx.net>
2013-06-21 20:44:11 +0200

    revision tag added

--- static/js/index.js
+++ static/js/index.js
@@ -2,6 +2,7 @@ var tagRegex = /#([a-z]+)/ig;
 var shortcodes =
 {
   "c": "chapter"
+  "r": "revision"
 }
 
 var acePostWriteDomLineHTML = function(hook, obj)
@@ -20,7 +21,7 @@ var acePostWriteDomLineHTML = function(hook, obj)
 
 function aceEditorCSS()
 {
-  return ["ep_chapterBold/static/css/chapterbold.css"];
+  return ["ep_chapterBold/static/css/linestyle.css"];
 }
 
 exports.acePostWriteDomLineHTML = acePostWriteDomLineHTML;
